209287b0aa6759fa4eb7fd601c3b3119a0e40ab5
NhanNPSE184696 <nguyenphucnhan2004@gmail.com>
2025-07-14 13:15:22 +0700

    feat: add Dead Screen

--- TheRevenantVengeance/Assets/Scripts/Controller/PlayerController.cs
+++ TheRevenantVengeance/Assets/Scripts/Controller/PlayerController.cs
@@ -50,6 +50,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private AudioClip attackClip;
     [SerializeField] private AudioClip takeHitClip;
     [SerializeField] private AudioClip ultiClip;
+    [SerializeField] private AudioClip deathClip;
 
     [SerializeField] private GameObject swordSpinPrefab;
     [SerializeField] private Transform spinCenter;
@@ -346,6 +347,10 @@ public class PlayerController : MonoBehaviour
     {
         // Kích hoạt animation chết
         animator.SetTrigger("Die");
+        if (audioSource != null && deathClip != null)
+        {
+            audioSource.PlayOneShot(deathClip);
+        }
         animator.SetBool("isDead", true);
 
         // Ngăn chặn các hành động khác của người chơi sau khi chết
